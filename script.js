f7611ee31b93d7d5d7ba4de73e8052f65af648f8
anushal78 <125723541+anushal78@users.noreply.github.com>
2023-11-03 22:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,177 @@
+const quizData = [
+{
+	question : "Which is the first dynesty of Karnataka?",
+	options : ["Vodeyar","Ganga","Kadamba","Shathavahana"],
+	answer : "Kadamba",
+},
+{
+	question : "Which is the capital of chalukya dynesty?",
+	options : ["Mysore","Banashankari","Basavakalyana","Badami"],
+	answer : "Badami",
+
+},
+{
+	question : "Which empire started the dasara festival in karnataka?",
+	options : ["Vodeyar","Vijayanagara","Hoysala","Nayaka"],
+	answer : "Vijayanagara",
+},
+{
+	question : "Who was ruling the chithradurga fort?",
+	options : ["Madakari Nayaka","Tippu Sulthan","Harshavardhana","Vishnuvardhana"],
+	answer : "Madakari Nayaka",
+},
+{
+	question : "Who built chennakeshava temple?",
+	options : ["Vishnuvardhana","Harshavardhana","Immadi Pulikeshi","Ballala"],
+	answer : "Vishnuvardhana",
+},
+{
+	question : "Who is the founder of Kadamba dynesty?",
+	options : ["Hakka bukka","Mayura","Rajaraja","Mummadi Krishnaraja"],
+	answer : "Mayura",
+},
+{
+	question : "Who wrote the great epic Mahabharatha?",
+	options : ["Valmeeki","Tulasidasa","Vedavyasa","Kabeera"],
+	answer : "Vedavyasa",
+},
+{
+	question : "Who is the father of Karnatic music?",
+	options : ["Kanakadasa","Purandaradasa","Tulasidasa","Shishunala Shareefha"],
+	answer : "Purandaradasa",
+},
+{
+	question : "Who is the first national poet from karnataka?",
+	options : ["Kuvempu","Govinda Pai","Shivarudrappa","Gireesh Karnad"],
+	answer : "Govinda Pai",
+},
+{
+	question : "Which river is called as Dakshina Ganga?",
+	options : ["Krishna","Kaveri","Tungabhadra","Arkavathi"],
+	answer : "Kaveri",
+},
+];
+
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('result');
+const submitButton = document.getElementById('submit');
+const retryButton = document.getElementById('retry');
+const showAnswerButton = document.getElementById('showAnswer');
+
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = [];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function displayQuestion() {
+  const questionData = quizData[currentQuestion];
+
+  const questionElement = document.createElement('div');
+  questionElement.className = 'question';
+  questionElement.innerHTML = questionData.question;
+
+  const optionsElement = document.createElement('div');
+  optionsElement.className = 'options';
+
+  const shuffledOptions = [...questionData.options];
+  shuffleArray(shuffledOptions);
+
+  for (let i = 0; i < shuffledOptions.length; i++) {
+    const option = document.createElement('label');
+    option.className = 'option';
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'quiz';
+    radio.value = shuffledOptions[i];
+
+    const optionText = document.createTextNode(shuffledOptions[i]);
+
+    option.appendChild(radio);
+    option.appendChild(optionText);
+    optionsElement.appendChild(option);
+  }
+
+  quizContainer.innerHTML = '';
+  quizContainer.appendChild(questionElement);
+  quizContainer.appendChild(optionsElement);
+}
+
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+  if (selectedOption) {
+    const answer = selectedOption.value;
+    if (answer === quizData[currentQuestion].answer) {
+      score++;
+    } else {
+      incorrectAnswers.push({
+        question: quizData[currentQuestion].question,
+        incorrectAnswer: answer,
+        correctAnswer: quizData[currentQuestion].answer,
+      });
+    }
+    currentQuestion++;
+    selectedOption.checked = false;
+    if (currentQuestion < quizData.length) {
+      displayQuestion();
+    } else {
+      displayResult();
+    }
+  }
+}
+
+function displayResult() {
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retryButton.style.display = 'inline-block';
+  showAnswerButton.style.display = 'inline-block';
+  resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+}
+
+function retryQuiz() {
+  currentQuestion = 0;
+  score = 0;
+  incorrectAnswers = [];
+  quizContainer.style.display = 'block';
+  submitButton.style.display = 'inline-block';
+  retryButton.style.display = 'none';
+  showAnswerButton.style.display = 'none';
+  resultContainer.innerHTML = '';
+  displayQuestion();
+}
+
+function showAnswer() {
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retryButton.style.display = 'inline-block';
+  showAnswerButton.style.display = 'none';
+
+  let incorrectAnswersHtml = '';
+  for (let i = 0; i < incorrectAnswers.length; i++) {
+    incorrectAnswersHtml += `
+      <p>
+        <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+        <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+        <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+      </p>
+    `;
+  }
+
+  resultContainer.innerHTML = `
+    <p>You scored ${score} out of ${quizData.length}!</p>
+    <p>Incorrect Answers:</p>
+    ${incorrectAnswersHtml}
+  `;
+}
+
+submitButton.addEventListener('click', checkAnswer);
+retryButton.addEventListener('click', retryQuiz);
+showAnswerButton.addEventListener('click', showAnswer);
+
+displayQuestion();
